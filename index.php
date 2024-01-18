d639fe2cd4ed79eb9a93cf3c66ce6a6335e94c02
TigerRanger <omerabba009@gmail.com>
2024-01-19 02:08:34 +0600

    second commit

--- index.php
+++ index.php
@@ -3,10 +3,38 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css?<?php echo time(); ?>">
+    <link rel="stylesheet" href="./css/menu.css?<?php echo time(); ?> ?>">
     <title>CSS Mega Menu type1</title>
 </head>
 <body>
     
+
+<div class="main_header">
+
+        <div class="container">
+            <div class="row">
+
+                <div class="col-md-3">
+						 <a href="#" class="logo" rel="home" itemprop="url">
+                            <img src="./images/Hikvision-Logo.svg" width=200px height=50px alt="Hikvision" />
+                        </a>               
+                </div>
+             
+                <div class="col-md-9">
+                  <div class="main-menu">
+                    <ul id="menu-main-menu" class="menu">
+                        <li  class="menu-item "><a href="#">Software</a></li>
+                        <li  class="menu-item "><a href="#">Website Development</a></li>
+                        <li  class="menu-item "><a href="#">Mobile Apps</a></li>
+                        <li  class="menu-item "><a href="#">SEO &amp; Marketing</a></li>
+                        <li  class="menu-item "><a href="#">Our Blogs</a></li>
+                        <li  class="menu-item "><a href="#">Company</a></li>
+                    </ul>
+                </div>               
+
+        </div>
+</div>
 
 
 
