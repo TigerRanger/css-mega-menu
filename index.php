c4141f74f008f15f327a23d83eb978f74ac9bfaf
TigerRanger <omerabba009@gmail.com>
2024-02-24 14:26:27 +0600

    basic css menu complete

--- index.php
+++ index.php
@@ -1,40 +1,69 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css?<?php echo time(); ?>">
-    <link rel="stylesheet" href="./css/menu.css?<?php echo time(); ?> ?>">
-    <title>CSS Mega Menu type1</title>
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CSS Menu</title>
+    <link rel="stylesheet" href="css/menu.css?v=<?php echo time(); ?> ?>">
 </head>
 <body>
-    
 
-<div class="main_header">
 
-        <div class="container">
-            <div class="row">
 
-                <div class="col-md-3">
-						 <a href="#" class="logo" rel="home" itemprop="url">
-                            <img src="./images/Hikvision-Logo.svg" width=200px height=70px alt="Hikvision" />
-                        </a>               
+<section class="menu_nz">
+    <div class="container nz">
+        <input class="sp_drop" type="checkbox" name="master_ceaser" id="master_ceaser" value="99">
+        <label class="opener_master" for="master_ceaser"><span class="icon-view-list"></span></label>
+
+        <div class="DDMenu box-mega">
+                <li class="hasChild realtive-m"><a href="https://gctlnetworking.com/server-rack-in-bangladesh.html">Server Rack</a><input class="sp_drop" type="checkbox" name="group" id="firstLv1" value="1"><label class="opener" for="firstLv1"></label><ul><li><a href="https://gctlnetworking.com/server-rack-in-bangladesh/wall-mount-server-rack.html">Wall Mount Server Rack</a></li><li><a href="https://gctlnetworking.com/server-rack-in-bangladesh/standing-server-rack.html">Standing Server Rack</a></li><li><a href="https://gctlnetworking.com/server-rack-in-bangladesh/network-server-rack.html">Network Server Rack </a></li><li><a href="https://gctlnetworking.com/server-rack-in-bangladesh/open-server-rack.html">Open Server Rack</a></li><li><a href="https://gctlnetworking.com/server-rack-in-bangladesh/kvm-console-monitor.html">KVM Console Monitor</a></li><li><a href="https://gctlnetworking.com/server-rack-in-bangladesh/server-rack-accessories.html">Server Rack Accessories</a></li><li><a href="https://gctlnetworking.com/server-rack-in-bangladesh/dateup-server-rack.html">DATEUP Server Rack </a></li><li><a href="https://gctlnetworking.com/server-rack-in-bangladesh/varito-server-rack.html">Varito Server Rack </a></li><li><a href="https://gctlnetworking.com/server-rack-in-bangladesh/toten-server-rack-cabinet.html">Toten Server rack cabinet</a></li></ul></li><li class="hasChild realtive-m"><a href="https://gctlnetworking.com/printer.html">Printer</a><input class="sp_drop" type="checkbox" name="group" id="firstLv2" value="1"><label class="opener" for="firstLv2"></label><ul><li><a href="https://gctlnetworking.com/printer/hp-printer.html">HP Printer</a></li></ul></li><li class="hasChild realtive-m"><a href="https://gctlnetworking.com/wired-networking.html">Wired Networking</a><input class="sp_drop" type="checkbox" name="group" id="firstLv3" value="1"><label class="opener" for="firstLv3"></label><ul><li><a href="https://gctlnetworking.com/wired-networking/networking-cable.html">Networking Cable</a></li><li><a href="https://gctlnetworking.com/wired-networking/modular.html">Modular</a></li><li><a href="https://gctlnetworking.com/wired-networking/patch-cord.html">Patch Cord</a></li><li><a href="https://gctlnetworking.com/wired-networking/patch-panel.html">Patch Panel</a></li><li><a href="https://gctlnetworking.com/wired-networking/faceplate.html">Faceplate</a></li><li><a href="https://gctlnetworking.com/wired-networking/networking-faceplate.html">Networking Faceplate</a></li><li><a href="https://gctlnetworking.com/wired-networking/keystone-modular-jack.html">Keystone Modular Jack</a></li></ul></li><li class="hasChild realtive-m"><a href="https://gctlnetworking.com/fiber-networking.html">Fiber Networking</a><input class="sp_drop" type="checkbox" name="group" id="firstLv4" value="1"><label class="opener" for="firstLv4"></label><ul><li><a href="https://gctlnetworking.com/fiber-networking/fiber-adaptor.html">Fiber Adaptor</a></li><li><a href="https://gctlnetworking.com/fiber-networking/fiber-patch-cord.html">Fiber Patch cord</a></li><li><a href="https://gctlnetworking.com/fiber-networking/fiber-pigtail.html">Fiber Pigtail</a></li><li><a href="https://gctlnetworking.com/fiber-networking/fiber-optic-cable.html">Fiber Optic Cable</a></li><li><a href="https://gctlnetworking.com/fiber-networking/fiber-patch-panel.html">Fiber Patch Panel</a></li><li><a href="https://gctlnetworking.com/fiber-networking/fiber-junction-box.html">Fiber Junction Box</a></li><li><a href="https://gctlnetworking.com/fiber-networking/media-converter.html">Media Converter</a></li></ul></li><li class="hasChild realtive-m"><a href="https://gctlnetworking.com/server.html">Server</a><input class="sp_drop" type="checkbox" name="group" id="firstLv5" value="1"><label class="opener" for="firstLv5"></label><ul><li><a href="https://gctlnetworking.com/server/computer-server.html">Computer Server</a></li><li><a href="https://gctlnetworking.com/server/hp-servers.html">HP Servers</a></li></ul></li>
+
+            <li class="mega-holder"><a class="hasChild realtive-m" href="https://gctlnetworking.com/switch-and-router.html">Switch and Router</a><input class="sp_drop" type="checkbox" name="group" id="firstLv6" value="1"><label class="opener" for="firstLv6"></label>
+                <div class="mega">
+                    <div class="mega-content">
+                        <div class="mg-group">
+                            <a href="https://gctlnetworking.com/switch-and-router/d-link-network-router.html">D-Link Network Router</a>
+                            <a href="https://gctlnetworking.com/switch-and-router/d-link-network-switch.html">D-Link Network Switch</a>
+                            <a href="https://gctlnetworking.com/switch-and-router/cisco-network-switch.html">Cisco Network Switch</a>
+                            <a href="https://gctlnetworking.com/switch-and-router/cisco-sfp-transceiver.html">Cisco SFP Transceiver</a>
+                            <a href="https://gctlnetworking.com/switch-and-router/cisco-access-point.html">Cisco Access Point</a>
+                        </div>
+
+                        <div class="mg-group">
+                            <a href="https://gctlnetworking.com/switch-and-router/d-link-network-router.html">D-Link Network Router</a>
+                            <a href="https://gctlnetworking.com/switch-and-router/d-link-network-switch.html">D-Link Network Switch</a>
+                            <a href="https://gctlnetworking.com/switch-and-router/cisco-network-switch.html">Cisco Network Switch</a>
+                            <a href="https://gctlnetworking.com/switch-and-router/cisco-sfp-transceiver.html">Cisco SFP Transceiver</a>
+                            <a href="https://gctlnetworking.com/switch-and-router/cisco-access-point.html">Cisco Access Point</a>
+                        </div>
+
+                        <div class="mg-group">
+                            <a href="https://gctlnetworking.com/switch-and-router/d-link-network-router.html">D-Link Network Router</a>
+                            <a href="https://gctlnetworking.com/switch-and-router/d-link-network-switch.html">D-Link Network Switch</a>
+                            <a href="https://gctlnetworking.com/switch-and-router/cisco-network-switch.html">Cisco Network Switch</a>
+                            <a href="https://gctlnetworking.com/switch-and-router/cisco-sfp-transceiver.html">Cisco SFP Transceiver</a>
+                            <a href="https://gctlnetworking.com/switch-and-router/cisco-access-point.html">Cisco Access Point</a>
+                        </div>
+
+                        <div class="mg-group">
+                            <a href="https://gctlnetworking.com/switch-and-router/d-link-network-router.html">D-Link Network Router</a>
+                            <a href="https://gctlnetworking.com/switch-and-router/d-link-network-switch.html">D-Link Network Switch</a>
+                            <a href="https://gctlnetworking.com/switch-and-router/cisco-network-switch.html">Cisco Network Switch</a>
+                            <a href="https://gctlnetworking.com/switch-and-router/cisco-sfp-transceiver.html">Cisco SFP Transceiver</a>
+                            <a href="https://gctlnetworking.com/switch-and-router/cisco-access-point.html">Cisco Access Point</a>
+                        </div>
+
+
+                    </div>
                 </div>
-             
-                <div class="col-md-9">
-                  <div class="main-menu">
-                    <ul id="menu-main-menu" class="menu">
-                        <li  class="menu-item "><a href="#">Software</a></li>
-                        <li  class="menu-item "><a href="#">Website Development</a></li>
-                        <li  class="menu-item "><a href="#">Mobile Apps</a></li>
-                        <li  class="menu-item "><a href="#">SEO &amp; Marketing</a></li>
-                        <li  class="menu-item "><a href="#">Our Blogs</a></li>
-                        <li  class="menu-item "><a href="#">Company</a></li>
-                    </ul>
-                </div>               
 
-        </div>
-</div>
+            </li><li class="hasChild realtive-m"><a href="https://gctlnetworking.com/operating-system.html">Operating System</a><input class="sp_drop" type="checkbox" name="group" id="firstLv7" value="1"><label class="opener" for="firstLv7"></label><ul><li><a href="https://gctlnetworking.com/operating-system/microsoft-windows-operating.html">Microsoft Windows Operating</a></li></ul></li>
+                <li><a href="https://gctlnetworking.com/blog/">Blog</a></li>
+        </ul>
+    </div>
+</section>
 
 
 
